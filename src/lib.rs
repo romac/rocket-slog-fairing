5cccde4e16aab4232bdba6dc726d26e9e9cf6fb1
Paul Woolcock <paul@woolcock.us>
2018-09-01 15:55:58 -0400

    crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,67 @@
+//! Slog fairing for Rocket.rs
+//!
+//! This module provides an easy way to use a slog `Logger` in a Rocket application
+//!
+//! # Usage
+//!
+//! To your Cargo.toml, add
+//!
+//!     [dependencies]
+//!     rocket-slog = { git = "https://github.com/pwoolcoc/rocket-slog" }
+//!
+//! In your rocket application, add
+//!
+//!     extern crate rocket_slog;
+//!     use rocket_slog::SlogFairing;
+//!
+//! Then, when you define your rocket.rs app, you need to do 3 things:
+//!
+//!   1. Build the slog `Logger` you want to use,
+//!   2. Disable Rocket's logging, and
+//!   3. Attach the SlogFairing to your application
+//!
+//! # Example
+//!
+//! ```rust,ignore
+//! #![feature(custom_derive, plugin)]
+//! #![plugin(rocket_codegen)]
+//!
+//! extern crate rocket;
+//! extern crate rocket_slog;
+//! #[macro_use(slog_o, slog_kv)] extern crate slog;
+//! extern crate slog_term;
+//! extern crate slog_async;
+//!
+//! use rocket::Config;
+//! use slog::Drain;
+//! use rocket_slog::SlogFairing;
+//!
+//! fn main() {
+//!     let decorator = slog_term::TermDecorator::new().build();
+//!     let drain = slog_term::FullFormat::new(decorator).build().fuse();
+//!     let drain = slog_async::Async::new(drain).build().fuse();
+//!     let logger = slog::Logger::root(drain, slog_o!("version" => env!("CARGO_PKG_VERSION")));
+//!
+//!     let fairing = SlogFairing::new(logger);
+//!
+//!     let config = Config::development().unwrap();
+//!     rocket::custom(config, false) // disables logging
+//!         .attach(fairing)
+//!         .launch();
+//! }
+//! ```
+//!
+//! The fairing also adds your logger to rocket's managed state, so you can do this in your routes
+//! too:
+//!
+//! ```rust,ignore
+//! use rocket_slog::SyncLogger;
+//!
+//! #[get("/")]
+//! fn index(log: SyncLogger) -> String {
+//!     
+//! }
+//! ```
 #[macro_use] extern crate slog;
 extern crate rocket;
 
